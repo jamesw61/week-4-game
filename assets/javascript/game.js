86891e13b3cbcc7809ec5053a5c72fa9088480c7
James Wick <jmwick2@hotmail.com>
2017-05-02 23:34:46 -0700

    added next enemyies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,11 @@
 var names = ["obiwan", "luke", "vader", "fett"];
 var healths = [120, 100, 150, 180];
 var attacks = [8, 5, 25, 20];
-var yourCharHealth = 0;
-var yourCharAttack = 0;
-var enemyCharHealth = 0;
-var enemyCharAttack = 0;
-var initialAttack = 0;
+var yourCharHealth = 1;
+var yourCharAttack = 1;
+var enemyCharHealth = 1;
+var enemyCharAttack = 1;
+var initialAttack = 1;
 
 $("#obiFoot").html(healths[0]);
 $("#skyFoot").html(healths[1]);
@@ -14,27 +14,39 @@ $("#vadFoot").html(healths[2]);
 $("#fettFoot").html(healths[3]);
 
 
-$(".characterBox").click(function()
-	{
+$(".characterBox").click(function(){
     	$(this).detach().appendTo("#yourChar").removeClass("characterBox").removeClass("notClicked").addClass("clickedCharacter");
     	$(".notClicked").detach().appendTo("#enemiesAvailable").css("background-color","red").addClass("possibleEnemies");
     	characterYouChose();   	
     	$( ".clickedCharacter > footer" ).html(yourCharHealth);
     	$(".characterBox").off("click"); 
-
-    	$(".possibleEnemies").click(function()
-     		{
-     			$(this).detach().appendTo("#defender").removeClass("notClicked").removeClass("possibleEnemies").addClass("clickedDefender").css("background-color","black").css("color","white");
-     			$(".possibleEnemies").off("click");
-     			enemyYouChose();	
-     			$( ".clickedDefender > footer" ).html(enemyCharHealth);
+    	makeEnemiesClickable();
+  //   	$(".possibleEnemies").click(function()
+  //    		{
+  //    			$(this).detach().appendTo("#defender").removeClass("notClicked").removeClass("possibleEnemies").addClass("clickedDefender").css("background-color","black").css("color","white");
+  //    			$(".possibleEnemies").off("click");
+  //    			enemyYouChose();	
+  //    			$( ".clickedDefender > footer" ).html(enemyCharHealth);
      				
-     				$("button").click(function(){
-     					fight();
-     					});
+  //    				$("button").click(function(){
+  //    					fight();
+  //    					console.log(enemyCharHealth);
 
-			});
-	});
+  //    				});
+		// });
+});
+
+function makeEnemiesClickable(){
+	$(".possibleEnemies").click(function(){
+     		$(this).detach().appendTo("#defender").removeClass("notClicked").removeClass("possibleEnemies").addClass("clickedDefender").css("background-color","black").css("color","white");
+     		$(".possibleEnemies").off("click");
+     		enemyYouChose();	
+     		$( ".clickedDefender > footer" ).html(enemyCharHealth);
+    			$("button").click(function(){
+     				fight();
+    			});
+		});
+}
 
 function characterYouChose(){
 	for (var i = 0; i<names.length; i++) {
@@ -61,6 +73,26 @@ function fight() {
 	yourCharAttack = yourCharAttack + initialAttack;
 	$( ".clickedCharacter > footer" ).html(yourCharHealth);
 	$( ".clickedDefender > footer" ).html(enemyCharHealth);
+	
+	if(enemyCharHealth <= 0){
+		yourCharHealth = yourCharHealth + enemyCharAttack;
+		$("#gameAlert").html("Chose Another Enemy");
+		$("button").off("click");
+		$(".clickedDefender").remove();
+		makeEnemiesClickable();
+	}
+
+
+	if(yourCharHealth <= 0){
+    	$("#gameAlert").html("You Lost<br><br><br>");
+    	$("button").off("click");
+    	var newButton = "<button>Restart Game</button>"; 
+    	$("#gameAlert").append(newButton).addClass(restartButton);
+    	$(".restartButton").click(function(){
+     		// restartGame();
+     	});
+	}
+	
 }
 
-
+// }
